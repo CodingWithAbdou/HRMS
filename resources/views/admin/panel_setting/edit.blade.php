eb7fc05e8ec37d09e0d8eb041d40e03b340a8bc3
CodingWithAbdou <coder.abdou@gmail.com>
2023-10-07 15:32:20 +0100

    - Create Tow Table finance_calenders & finance_cln_periods & With model And Controller

--- resources/views/admin/panel_setting/edit.blade.php
+++ resources/views/admin/panel_setting/edit.blade.php
@@ -8,7 +8,7 @@
     <section class="pt-4 col-12">
     <div class="card" >
         <div class="card-header"  >
-            <h3 class="card-title card_title_center"> تحديث بيانات الضبط العام </h3>
+            <h3 class="card-title card_title_center  py-2 fs-3"> تحديث بيانات الضبط العام </h3>
         </div>
         @if(@isset($data) and !@empty($data))
         <div class="card-body">
@@ -16,140 +16,140 @@
                 <form action="{{route('general_settings_update')}}" method="POST">
                 @csrf
                     <tr class="">
-                        <th class="w-30 p-4">اسم الشركة</th>
+                        <th class="w-30 py-2">اسم الشركة</th>
                         <th class="w-70">
-                            <input name="name" class="form-control mt-2" type="text"  value="{{$data->name}}">
+                            <input name="name" class="form-control mt-1" type="text"  value="{{$data->name}}">
                             @error('name') <span class="text-danger">{{ $message }}</span> @enderror
                         </th>
                     </tr>
 
 
                     <tr>
-                        <th class="w-30 p-4">هاتف الشركة</th>
+                        <th class="w-30 py-2">هاتف الشركة</th>
                         <th class="w-70">
-                            <input  name="phones" class="form-control mt-2  " type="tel"  value="{{$data->phones}}">
+                            <input  name="phones" class="form-control mt-1  " type="tel"  value="{{$data->phones}}">
                             @error('phones') <span class="text-danger">{{ $message }}</span> @enderror
                         </th>
 
                     </tr>
 
                     <tr>
-                        <th class="w-30 p-4">عنوان الشركة</th>
+                        <th class="w-30 py-2">عنوان الشركة</th>
                         <th class="w-70">
-                            <input name="address" class="form-control mt-2" type="tel"  value="{{$data->address}}">
+                            <input name="address" class="form-control mt-1" type="tel"  value="{{$data->address}}">
                             @error('address') <span class="text-danger">{{ $message }}</span> @enderror
                         </th>
                     </tr>
 
                     <tr>
-                        <th class="w-30 p-4">ايميل الشركة</th>
+                        <th class="w-30 py-2">ايميل الشركة</th>
                         <th class="w-70">
-                            <input name="email" class="form-control mt-2" type="tel"  value="{{$data->email}}">
+                            <input name="email" class="form-control mt-1" type="tel"  value="{{$data->email}}">
                             @error('email') <span class="text-danger">{{ $message }}</span> @enderror
                         </th>
                     </tr>
 
                     <tr>
-                        <th class="w-30 p-4">بعد كم دقيقة تحسب تاخير الحضور</th>
+                        <th class="w-30 py-2">بعد كم دقيقة تحسب تاخير الحضور</th>
                         <th class="w-70">
-                            <input name="after_minute_calculate_delay" class="form-control mt-2" type="tel"  value="{{$data->after_minute_calculate_delay}}">
+                            <input name="after_minute_calculate_delay" class="form-control mt-1" type="tel"  value="{{$data->after_minute_calculate_delay}}">
                             @error('after_minute_calculate_delay') <span class="text-danger">{{ $message }}</span> @enderror
                         </th>
                     </tr>
 
                     <tr>
-                        <th class="w-30 p-4">بعد كم تحسب الانصراف المبكر</th>
+                        <th class="w-30 py-2">بعد كم تحسب الانصراف المبكر</th>
                         <th class="w-70">
-                            <input name="after_minute_calculate_early_departure" class="form-control mt-2" type="tel"  value="{{$data->after_minute_calculate_early_departure}}">
+                            <input name="after_minute_calculate_early_departure" class="form-control mt-1" type="tel"  value="{{$data->after_minute_calculate_early_departure}}">
                             @error('after_minute_calculate_early_departure') <span class="text-danger">{{ $message }}</span> @enderror
                         </th>
 
                     </tr>
 
                     <tr>
-                        <th class="w-30 p-4">بعد كم دقيقة مجموع انصراف مبكر او حضور متاخر نخصم ربع يوم</th>
+                        <th class="w-30 py-2">بعد كم دقيقة مجموع انصراف مبكر او حضور متاخر نخصم ربع يوم</th>
                         <th class="w-70">
-                            <input name="after_minute_quarterday" class="form-control mt-2" type="tel"  value="{{$data->after_minute_quarterday}}">
+                            <input name="after_minute_quarterday" class="form-control mt-1" type="tel"  value="{{$data->after_minute_quarterday}}">
                             @error('after_minute_quarterday') <span class="text-danger">{{ $message }}</span> @enderror
                         </th>
                     </tr>
 
                     <tr>
-                        <th class="w-30 p-4">بعد كم مرة انصراف مبكر او حضور متاخر نخصم نصف يوم</th>
+                        <th class="w-30 py-2">بعد كم مرة انصراف مبكر او حضور متاخر نخصم نصف يوم</th>
                         <th class="w-70">
-                            <input name="after_time_half_daycut" class="form-control mt-2" type="tel"  value="{{$data->after_time_half_daycut}}">
+                            <input name="after_time_half_daycut" class="form-control mt-1" type="tel"  value="{{$data->after_time_half_daycut}}">
                             @error('after_time_half_daycut') <span class="text-danger">{{ $message }}</span> @enderror
                         </th>
                     </tr>
 
                     <tr>
-                        <th class="w-30 p-4"> بعد كم مرة انصراف مبكر او حضور متاخر نخصم  يوم كامل</th>
+                        <th class="w-30 py-2"> بعد كم مرة انصراف مبكر او حضور متاخر نخصم  يوم كامل</th>
                         <th class="w-70">
-                            <input name="after_time_allday_daycut" class="form-control mt-2" type="tel"  value="{{$data->after_time_allday_daycut}}">
+                            <input name="after_time_allday_daycut" class="form-control mt-1" type="tel"  value="{{$data->after_time_allday_daycut}}">
                             @error('after_time_allday_daycut') <span class="text-danger">{{ $message }}</span> @enderror
                         </th>
                     </tr>
 
                     <tr>
-                        <th class="w-30 p-4">رصيد اجازات الموضف الشهري</th>
+                        <th class="w-30 py-2">رصيد اجازات الموضف الشهري</th>
                         <th class="w-70">
-                            <input name="mounthly_vacation_blance" class="form-control mt-2" type="tel"  value="{{$data->mounthly_vacation_blance}}">
+                            <input name="mounthly_vacation_blance" class="form-control mt-1" type="tel"  value="{{$data->mounthly_vacation_blance}}">
                             @error('mounthly_vacation_blance') <span class="text-danger">{{ $message }}</span> @enderror
                         </th>
                     </tr>
 
                     <tr>
-                        <th class="w-30 p-4">بعد كم يوم ينزل للموضف رصيد اجازات</th>
+                        <th class="w-30 py-2">بعد كم يوم ينزل للموضف رصيد اجازات</th>
                         <th class="w-70">
-                            <input name="after_days_begin_vacation" class="form-control mt-2" type="tel"  value="{{$data->after_days_begin_vacation}}">
+                            <input name="after_days_begin_vacation" class="form-control mt-1" type="tel"  value="{{$data->after_days_begin_vacation}}">
                             @error('after_days_begin_vacation') <span class="text-danger">{{ $message }}</span> @enderror
                         </th>
                     </tr>
 
                     <tr>
-                        <th class="w-30 p-4">عدد الاجازات للموضف مثل نزول عشرة ايام ونص بغد ستة شهور للموضف</th>
+                        <th class="w-30 py-2">عدد الاجازات للموضف مثل نزول عشرة ايام ونص بغد ستة شهور للموضف</th>
                         <th class="w-70">
-                            <input name="first_balance_begin_vication" class="form-control mt-2" type="tel"  value="{{$data->first_balance_begin_vication}}">
+                            <input name="first_balance_begin_vication" class="form-control mt-1" type="tel"  value="{{$data->first_balance_begin_vication}}">
                             @error('first_balance_begin_vication') <span class="text-danger">{{ $message }}</span> @enderror
                         </th>
                     </tr>
 
                     <tr>
-                        <th class="w-30 p-4">بعد كم يوم ينزل للموضف رصيد اجازات </th>
+                        <th class="w-30 py-2">بعد كم يوم ينزل للموضف رصيد اجازات </th>
                         <th class="w-70">
-                            <input name="after_day_begin_vaction" class="form-control mt-2" type="tel"  value="{{$data->after_day_begin_vaction}}">
+                            <input name="after_day_begin_vaction" class="form-control mt-1" type="tel"  value="{{$data->after_day_begin_vaction}}">
                             @error('after_day_begin_vaction') <span class="text-danger">{{ $message }}</span> @enderror
                         </th>
                     </tr>
 
                     <tr>
-                        <th class="w-30 p-4">قيمة خصم ايام بعد اول مرة عياب بدون ادن</th>
+                        <th class="w-30 py-2">قيمة خصم ايام بعد اول مرة عياب بدون ادن</th>
                         <th class="w-70">
-                            <input name="sanctions_value_first_abcence" class="form-control mt-2" type="tel"  value="{{$data->sanctions_value_first_abcence}}">
+                            <input name="sanctions_value_first_abcence" class="form-control mt-1" type="tel"  value="{{$data->sanctions_value_first_abcence}}">
                             @error('sanctions_value_first_abcence') <span class="text-danger">{{ $message }}</span> @enderror
                         </th>
                     </tr>
 
                     <tr>
-                        <th class="w-30 p-4">قيمة خصم ايام بعد ثاني مرة عياب بدون ادن</th>
+                        <th class="w-30 py-2">قيمة خصم ايام بعد ثاني مرة عياب بدون ادن</th>
                         <th class="w-70">
-                            <input name="sanctions_value_second_abcence" class="form-control mt-2" type="tel"  value="{{$data->sanctions_value_second_abcence}}">
+                            <input name="sanctions_value_second_abcence" class="form-control mt-1" type="tel"  value="{{$data->sanctions_value_second_abcence}}">
                             @error('sanctions_value_second_abcence') <span class="text-danger">{{ $message }}</span> @enderror
                         </th>
                     </tr>
 
                     <tr>
-                        <th class="w-30 p-4">قيمة خصم ايام بعد ثالث مرة عياب بدون ادن</th>
+                        <th class="w-30 py-2">قيمة خصم ايام بعد ثالث مرة عياب بدون ادن</th>
                         <th class="w-70">
-                            <input name="sanctions_value_thaird_abcence" class="form-control mt-2" type="tel"  value="{{$data->sanctions_value_thaird_abcence}}">
+                            <input name="sanctions_value_thaird_abcence" class="form-control mt-1" type="tel"  value="{{$data->sanctions_value_thaird_abcence}}">
                             @error('sanctions_value_thaird_abcence') <span class="text-danger">{{ $message }}</span> @enderror
                         </th>
                     </tr>
 
                     <tr>
-                        <th class="w-30 p-4">قيمة خصم ايام بعد رابع مرة عياب بدون ادن</th>
+                        <th class="w-30 py-2">قيمة خصم ايام بعد رابع مرة عياب بدون ادن</th>
                         <th class="w-70">
-                            <input name="sanctions_value_fourth_abcence" class="form-control mt-2" type="tel"  value="{{$data->sanctions_value_fourth_abcence}}">
+                            <input name="sanctions_value_fourth_abcence" class="form-control mt-1" type="tel"  value="{{$data->sanctions_value_fourth_abcence}}">
                             @error('sanctions_value_fourth_abcence') <span class="text-danger">{{ $message }}</span> @enderror
                         </th>
 
--- resources/views/admin/panel_setting/index.blade.php
+++ resources/views/admin/panel_setting/index.blade.php
@@ -8,99 +8,99 @@
 <section class="pt-4 col-12">
     <div class="card" >
         <div class="card-header"  >
-            <h3 class="card-title card_title_center">بيانات الضبط العام</h3>
+            <h3 class="card-title card_title_center py-2 fw-bold">بيانات الضبط العام</h3>
         </div>
         @if(@isset($data) and !@empty($data))
         <div class="card-body">
             <table  class="table table-bordered table-hover" style="width:100% ">
                 <tr class="jsgrid-header-row">
-                    <th class="w-30 p-4">اسم الشركة</th>
-                    <th class="w-70 p-4 text-secondary-emphasis fw-normal">{{$data->name}}</th>
+                    <th class="w-30 p-2">اسم الشركة</th>
+                    <th class="w-70 p-2 text-secondary-emphasis fw-normal">{{$data->name}}</th>
                 </tr>
 
                 <tr>
-                    <th class="w-30 p-4">حالة التفعيل</th>
-                    <th class="w-70 p-4 text-secondary-emphasis fw-normal">{{$data->system_status == 1 ? "مفعل" : "غير مفعلة"}}</th>
+                    <th class="w-30 p-2">حالة التفعيل</th>
+                    <th class="w-70 p-2 text-secondary-emphasis fw-normal">{{$data->system_status == 1 ? "مفعل" : "غير مفعلة"}}</th>
                 </tr>
 
                 <tr>
-                    <th class="w-30 p-4">هاتف الشركة</th>
-                    <th class="w-70 p-4 text-secondary-emphasis fw-normal">{{$data->phones}}</th>
+                    <th class="w-30 p-2">هاتف الشركة</th>
+                    <th class="w-70 p-2 text-secondary-emphasis fw-normal">{{$data->phones}}</th>
                 </tr>
 
                 <tr>
-                    <th class="w-30 p-4">عنوان الشركة</th>
-                    <th class="w-70 p-4 text-secondary-emphasis fw-normal">{{$data->address}}</th>
+                    <th class="w-30 p-2">عنوان الشركة</th>
+                    <th class="w-70 p-2 text-secondary-emphasis fw-normal">{{$data->address}}</th>
                 </tr>
 
                 <tr>
-                    <th class="w-30 p-4">ايميل الشركة</th>
-                    <th class="w-70 p-4 text-secondary-emphasis fw-normal">{{$data->email}}</th>
+                    <th class="w-30 p-2">ايميل الشركة</th>
+                    <th class="w-70 p-2 text-secondary-emphasis fw-normal">{{$data->email}}</th>
                 </tr>
 
                 <tr>
-                    <th class="w-30 p-4">بعد كم دقيقة تحسب تاخير الحضور</th>
-                    <th class="w-70 p-4 text-secondary-emphasis fw-normal">{{$data->after_minute_calculate_delay}}</th>
+                    <th class="w-30 p-2">بعد كم دقيقة تحسب تاخير الحضور</th>
+                    <th class="w-70 p-2 text-secondary-emphasis fw-normal">{{$data->after_minute_calculate_delay}}</th>
                 </tr>
 
                 <tr>
-                    <th class="w-30 p-4">بعد كم تحسب الانصراف المبكر</th>
-                    <th class="w-70 p-4 text-secondary-emphasis fw-normal">{{$data->after_minute_calculate_early_departure}}</th>
+                    <th class="w-30 p-2">بعد كم تحسب الانصراف المبكر</th>
+                    <th class="w-70 p-2 text-secondary-emphasis fw-normal">{{$data->after_minute_calculate_early_departure}}</th>
                 </tr>
 
                 <tr>
-                    <th class="w-30 p-4">بعد كم دقيقة مجموع انصراف مبكر او حضور متاخر نخصم ربع يوم</th>
-                    <th class="w-70 p-4 text-secondary-emphasis fw-normal">{{$data->after_minute_quarterday}}</th>
+                    <th class="w-30 p-2">بعد كم دقيقة مجموع انصراف مبكر او حضور متاخر نخصم ربع يوم</th>
+                    <th class="w-70 p-2 text-secondary-emphasis fw-normal">{{$data->after_minute_quarterday}}</th>
                 </tr>
 
                 <tr>
-                    <th class="w-30 p-4">بعد كم مرة انصراف مبكر او حضور متاخر نخصم نصف يوم</th>
-                    <th class="w-70 p-4 text-secondary-emphasis fw-normal">{{$data->after_time_half_daycut}}</th>
+                    <th class="w-30 p-2">بعد كم مرة انصراف مبكر او حضور متاخر نخصم نصف يوم</th>
+                    <th class="w-70 p-2 text-secondary-emphasis fw-normal">{{$data->after_time_half_daycut}}</th>
                 </tr>
 
                 <tr>
-                    <th class="w-30 p-4"> بعد كم مرة انصراف مبكر او حضور متاخر نخصم  يوم كامل</th>
-                    <th class="w-70 p-4 text-secondary-emphasis fw-normal">{{$data->after_time_allday_daycut}}</th>
+                    <th class="w-30 p-2"> بعد كم مرة انصراف مبكر او حضور متاخر نخصم  يوم كامل</th>
+                    <th class="w-70 p-2 text-secondary-emphasis fw-normal">{{$data->after_time_allday_daycut}}</th>
                 </tr>
 
                 <tr>
-                    <th class="w-30 p-4">رصيد اجازات الموضف الشهري</th>
-                    <th class="w-70 p-4 text-secondary-emphasis fw-normal">{{$data->mounthly_vacation_blance}}</th>
+                    <th class="w-30 p-2">رصيد اجازات الموضف الشهري</th>
+                    <th class="w-70 p-2 text-secondary-emphasis fw-normal">{{$data->mounthly_vacation_blance}}</th>
                 </tr>
 
                 <tr>
-                    <th class="w-30 p-4">بعد كم يوم ينزل للموضف رصيد اجازات</th>
-                    <th class="w-70 p-4 text-secondary-emphasis fw-normal">{{$data->after_days_begin_vacation}}</th>
+                    <th class="w-30 p-2">بعد كم يوم ينزل للموضف رصيد اجازات</th>
+                    <th class="w-70 p-2 text-secondary-emphasis fw-normal">{{$data->after_days_begin_vacation}}</th>
                 </tr>
 
                 <tr>
-                    <th class="w-30 p-4">عدد الاجازات للموضف مثل نزول عشرة ايام ونص بغد ستة شهور للموضف</th>
-                    <th class="w-70 p-4 text-secondary-emphasis fw-normal">{{$data->first_balance_begin_vication}}</th>
+                    <th class="w-30 p-2">عدد الاجازات للموضف مثل نزول عشرة ايام ونص بغد ستة شهور للموضف</th>
+                    <th class="w-70 p-2 text-secondary-emphasis fw-normal">{{$data->first_balance_begin_vication}}</th>
                 </tr>
 
                 <tr>
-                    <th class="w-30 p-4">بعد كم يوم ينزل للموضف رصيد اجازات </th>
-                    <th class="w-70 p-4 text-secondary-emphasis fw-normal">{{$data->first_balance_begin_vication}}</th>
+                    <th class="w-30 p-2">بعد كم يوم ينزل للموضف رصيد اجازات </th>
+                    <th class="w-70 p-2 text-secondary-emphasis fw-normal">{{$data->first_balance_begin_vication}}</th>
                 </tr>
 
                 <tr>
-                    <th class="w-30 p-4">قيمة خصم ايام بعد اول مرة عياب بدون ادن</th>
-                    <th class="w-70 p-4 text-secondary-emphasis fw-normal">{{$data->sanctions_value_first_abcence}}</th>
+                    <th class="w-30 p-2">قيمة خصم ايام بعد اول مرة عياب بدون ادن</th>
+                    <th class="w-70 p-2 text-secondary-emphasis fw-normal">{{$data->sanctions_value_first_abcence}}</th>
                 </tr>
 
                 <tr>
-                    <th class="w-30 p-4">قيمة خصم ايام بعد ثاني مرة عياب بدون ادن</th>
-                    <th class="w-70 p-4 text-secondary-emphasis fw-normal">{{$data->sanctions_value_second_abcence}}</th>
+                    <th class="w-30 p-2">قيمة خصم ايام بعد ثاني مرة عياب بدون ادن</th>
+                    <th class="w-70 p-2 text-secondary-emphasis fw-normal">{{$data->sanctions_value_second_abcence}}</th>
                 </tr>
 
                 <tr>
-                    <th class="w-30 p-4">قيمة خصم ايام بعد ثالث مرة عياب بدون ادن</th>
-                    <th class="w-70 p-4 text-secondary-emphasis fw-normal">{{$data->sanctions_value_thaird_abcence}}</th>
+                    <th class="w-30 p-2">قيمة خصم ايام بعد ثالث مرة عياب بدون ادن</th>
+                    <th class="w-70 p-2 text-secondary-emphasis fw-normal">{{$data->sanctions_value_thaird_abcence}}</th>
                 </tr>
 
                 <tr>
-                    <th class="w-30 p-4">قيمة خصم ايام بعد رابع مرة عياب بدون ادن</th>
-                    <th class="w-70 p-4 text-secondary-emphasis fw-normal">{{$data->sanctions_value_fourth_abcence}}</th>
+                    <th class="w-30 p-2">قيمة خصم ايام بعد رابع مرة عياب بدون ادن</th>
+                    <th class="w-70 p-2 text-secondary-emphasis fw-normal">{{$data->sanctions_value_fourth_abcence}}</th>
                 </tr>
 
                 <tr >
